00d653abd2e61f7956b092472a6c6128b56a9309
sder12 <sara.cetto@gmail.com>
2022-11-28 15:51:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+var_dump('Hello debug')
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title>PHP Strong Password Generator</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Strong Password Generator</h1>
+            <h2>Genera una password sicura </h2>
+        </header>
+        <main>
+            <p>Lunghezza password: </p>
+        </main>
+    </div>
+</body>
+
+</html>
