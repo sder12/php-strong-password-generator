cecc7c2c7f49f8f04a8ea3ed00fe03846969b02f
sder12 <sara.cetto@gmail.com>
2022-11-28 16:34:44 +0100

    Password arrays

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php
-$length_password = intval($_GET['passwordlength']) ?? "";
+$length_password = $_GET['passwordlength'] ?? "";
 var_dump($length_password);
+
+
+//alphabet
+$pass_alphabet = array_merge(range('A', 'Z'), range('a', 'z'));
+//symbols
+$pass_symbols = [];
+$symbols = "~`!@#$%^&*()_-+={[}]|\:;<,>.?/";
+foreach (str_split($symbols) as $symbol) {
+    $pass_symbols[] = $symbol;
+}
+//numbers
+$pass_numbers = [];
+$pass_num = range(0, 9);
+foreach ($pass_num as $num) {
+    $number = strval($num);
+    $pass_numbers[] = $number;
+}
+
+//ALPHABET + NUMBERS + SYMBOLS
+$password_characters = array_merge($pass_alphabet, $pass_numbers, $pass_symbols);
+var_dump($password_characters);
+
+
 ?>
 
 <!DOCTYPE html>
